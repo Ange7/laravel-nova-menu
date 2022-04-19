bba6f20c352377a45b4dedf75cdd59bb7c729e9d
Alex <russo@novius.fr>
2022-04-19 15:12:02 +0200

    Mise à jours laravel-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,18 @@
 let mix = require('laravel-mix')
 
 mix.setPublicPath('dist')
-   .js('resources/js/tool.js', 'js')
+   .js('resources/js/tool.js', 'js').vue({ version: 3 }).webpackConfig({
+    externals: {
+        vue: 'Vue',
+    },
+    output: {
+        uniqueName: 'vendor/package',
+    }
+    })
    .sass('resources/sass/tool.scss', 'css')
+
+let path = require('path')
+
+mix.alias({
+    'laravel-nova': path.join(__dirname, '../../vendor/laravel/nova/resources/js/mixins/packages.js'),
+})
